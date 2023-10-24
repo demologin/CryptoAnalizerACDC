c110b04feef3bfb2544c5d09265432a97552687c
akhmelev <akhmelev@gmail.com>
2023-10-24 14:36:00 +0300

    FIX: update files: README.MD pom.xml .gitignore

--- src/main/java/com/javarush/ivanov/ConsoleRunner.java
+++ src/main/java/com/javarush/ivanov/ConsoleRunner.java
@@ -2,6 +2,8 @@ package com.javarush.ivanov;
 
 public class ConsoleRunner {
     public static void main(String[] args) {
-        //Create package with your lastname in com.javarush and add your solution
+        // Create package with your lastname in com.javarush
+        // and add your solution like this
+        System.out.println("у вас будет такой же пакет, но со своим решением");
     }
 }
