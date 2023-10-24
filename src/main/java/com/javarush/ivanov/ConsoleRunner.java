a887319f04ccc494e5a03c81ee055ca98dff14b6
akhmelev <akhmelev@gmail.com>
2023-10-24 13:58:42 +0300

    ADD: ivanov sample package

---
+++ src/main/java/com/javarush/ivanov/ConsoleRunner.java
@@ -0,0 +1,7 @@
+package com.javarush.ivanov;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        //Create package with your lastname in com.javarush and add your solution
+    }
+}
