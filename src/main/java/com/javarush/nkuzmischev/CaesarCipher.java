37508132a836f476092fe3fcc4c1a2dfa474c269
nakuzmisch <nkuzmisch@mts.ru>
2023-10-29 14:31:49 +0300

    Чтение/запись из файла + русский алфавит

--- src/main/java/com/javarush/nkuzmischev/CaesarCipher.java
+++ src/main/java/com/javarush/nkuzmischev/CaesarCipher.java
@@ -1,23 +1,56 @@
 package com.javarush.nkuzmischev;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
-import java.util.Scanner;
+
 
 public class CaesarCipher {
     public static void main(String[] args) {
+        char[] russianAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', ':', '!', '?', ' '};
+        int shift = getShiftFromConsole();
+        String inputFilename = "input.txt";
+        String outputFilename = "output.txt";
+
+        try {
+            String plaintext = readFromFile(inputFilename);
+            String ciphertext = encrypt(plaintext, shift, russianAlphabet);
+            writeToFile(outputFilename, ciphertext);
+            System.out.println("Шифрованный текст успешно записан в файл " + outputFilename);
+
+            String decryptedText = decrypt(ciphertext, shift, russianAlphabet);
+            System.out.println("Расшифрованный текст: " + decryptedText);
+        } catch (IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+    //функция getShiftFromConsole(), позволяет пользователю ввести размер сдвига с консоли
+    public static int getShiftFromConsole() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите текст для шифрования: ");
-        String plaintext = scanner.nextLine();
-        System.out.print("Введите сдвиг: ");
+        System.out.print("Введите размер сдвига: ");
         int shift = scanner.nextInt();
+        scanner.close();
+        return shift;
+    }
+    //Для работы с файлами используются методы readFromFile и writeToFile, которые считывают данные из файла и записывают данные в файл соответственно.
+    public static String readFromFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        reader.close();
+        return stringBuilder.toString();
+    }
 
-        char[] russianAlphabet = {'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
-        String ciphertext = encrypt(plaintext, shift, russianAlphabet);
-        System.out.println("Зашифрованный текст: " + ciphertext);
-
-        String decryptedText = decrypt(ciphertext, shift, russianAlphabet);
-        System.out.println("Расшифрованный текст: " + decryptedText);
+    public static void writeToFile(String filename, String content) throws IOException {
+        FileWriter writer = new FileWriter(filename);
+        writer.write(content);
+        writer.close();
     }
 
     public static String encrypt(String plaintext, int shift, char[] alphabet) {
