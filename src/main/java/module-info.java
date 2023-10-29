ac39deed45537289424194704131a16ce5a2d3b3
akhmelev <akhmelev@gmail.com>
2023-10-30 02:39:00 +0300

    ADD Demo FX without @FXML

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.javarush.khmelov {
+    requires javafx.controls; //javafx
+    requires info.picocli;    //picocli
+    requires java.desktop;    //swing
+    exports com.javarush.khmelov;
+}
