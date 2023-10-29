b0b54a658ef1df36b22a0fe651febe1587f9a30b
Matvei Zhukov <zhuk5700@mail.ru>
2023-10-29 17:15:52 +0300

    Create directory zhukov. Сreation alphabet,decode and encode process.

--- src/main/java/com/javarush/zhukov/constant/AlphaBet.java
+++ src/main/java/com/javarush/zhukov/constant/AlphaBet.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class AlphaBet {
     private static final String en = "qwertyuiopasdfghjklzxcvbnm";
     private static final String rus = "йцукенгшщзхъэждлорпавыфячсмитьбю";
-    private static final String symblos = ".,\"\':;-?! ";
+    private static final String symblos = ".,\"':;-?! ";
     private static final String numbers = "0123456789";
     public static final char[] arrayAlphaBet = (en+rus+numbers+symblos).toCharArray();
     public static Map<Character, Integer> alphaBet = new HashMap<>();
