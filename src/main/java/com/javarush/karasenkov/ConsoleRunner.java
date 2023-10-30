44692dd279b8488d57d2663daf1e854a62913660
bvkarasy <bvkarasy@mts.ru>
2023-10-30 13:41:47 +0300

    add Decoder class and made coding logic in a separate class.

--- src/main/java/com/javarush/karasenkov/ConsoleRunner.java
+++ src/main/java/com/javarush/karasenkov/ConsoleRunner.java
@@ -1,56 +1,24 @@
 package com.javarush.karasenkov;
 
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class ConsoleRunner {
-    public static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!'};
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Выберите режим работы программы и введите нужную цифру :");
         System.out.println("1. Кодирование");
         System.out.println("2. Декодирование");
         int workBranch = scanner.nextInt();
-        if ( workBranch == 1 ) {
-            System.out.println("Укажите полный путь до исходного файла");
-            String src = "/Users/bkarasenkov/IdeaProjects/CryptoAnalizerACDC/text/text.txt";
-            //String src = scanner.next();
-
-            System.out.println("Укажите полный путь до файла с результатами");
-            String dest = "/Users/bkarasenkov/IdeaProjects/CryptoAnalizerACDC/text/result.txt";
-            //String dest = scanner.next();
-
-            System.out.println("Укажите номер ключа шифра в виде числа");
-            int cipherKey = scanner.nextInt();
-
-            try(FileReader reader = new FileReader(src);
-                FileWriter writer = new FileWriter(dest))
-            {
-                char[] buffer = new char[65536];
-                while (reader.ready())
-                {
-                    int real = reader.read(buffer);
-                    for (int i = 0; i < real; i++) {
-                        char letter = buffer[i];
-                        char lowerLetter = Character.toLowerCase(letter);
-                        for (int j = 0; j < ALPHABET.length; j++) {
-                            if (lowerLetter == ALPHABET[j]) {
-                                if (j + cipherKey >= ALPHABET.length) {
-                                    int newIndex = (j + cipherKey) % ALPHABET.length;
-                                    char newLetter = ALPHABET[newIndex];
-                                    buffer[i] = newLetter;
-                                    continue;
-                                }
-                                char newLetter = ALPHABET[j + cipherKey];
-                                buffer[i] = newLetter;
-                            }
-                        }
-                    }
-                    writer.write(buffer, 0, real);
-                }
+        switch (workBranch) {
+            case 1: {
+                Coder.codingText();
+                break;
+            }
+            case 2: {
+                Decoder.decodeText();
+                break;
             }
         }
     }
---
+++ src/main/java/com/javarush/karasenkov/Decoder.java
@@ -0,0 +1,53 @@
+package com.javarush.karasenkov;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Decoder {
+    public static final char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', ' '};
+    public static void decodeText() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Укажите полный путь до исходного файла (закодированный)");
+        String src = "/Users/bkarasenkov/IdeaProjects/CryptoAnalizerACDC/text/result.txt";
+        //String src = scanner.next();
+
+        System.out.println("Укажите полный путь до файла с результатами (раскодированный)");
+        String dest = "/Users/bkarasenkov/IdeaProjects/CryptoAnalizerACDC/text/decoded_text.txt";
+        //String dest = scanner.next();
+
+        System.out.println("Укажите номер ключа шифра в виде числа");
+        int cipherKey = scanner.nextInt();
+        if (cipherKey > ALPHABET.length) {
+            cipherKey = cipherKey % ALPHABET.length;
+        }
+        try (FileReader reader = new FileReader(src);
+             FileWriter writer = new FileWriter(dest)) {
+            char[] buffer = new char[65536];
+            while (reader.ready()) {
+                int real = reader.read(buffer);
+                for (int i = 0; i < real; i++) {
+                    char letter = buffer[i];
+                    char lowLetter = Character.toLowerCase(letter);
+                    for (int j = 0; j < ALPHABET.length; j++) {
+                        if (lowLetter == ALPHABET[j]) {
+                            if (j - cipherKey < 0) {
+                                char newLetter = ALPHABET[ALPHABET.length - (cipherKey - j)];
+                                buffer[i] = newLetter;
+                                continue;
+                            }
+                            char newLetter = ALPHABET[j - cipherKey];
+                            buffer[i] = newLetter;
+                        }
+                    }
+                }
+                writer.write(buffer, 0, real);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
