d91d919959a437dcaad2e9d3e90a1c6b781fb8e0
boyarinovLex <boyarinov.lex@ya.ru>
2023-10-30 20:38:39 +0700

    FIX setKey

--- src/main/java/com/javarush/boyarinov/Start.java
+++ src/main/java/com/javarush/boyarinov/Start.java
@@ -4,6 +4,7 @@ package com.javarush.boyarinov;
 import com.javarush.boyarinov.constats.Constant;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,20 +13,29 @@ public class Start {
     public static void main(String[] args) {
 
         CreatePath path = new CreatePath();
-        try (BufferedReader bufferedReader = Files.newBufferedReader(path.getPath("mytext"))) {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path.getPath("text.txt"));
+             BufferedWriter bufferedWriter = Files.newBufferedWriter(path.getPath("encryptText.txt"))) {
             while (bufferedReader.ready()) {
                 int read = bufferedReader.read();
-                int setKey = 3;
+                int setKey = 53;
                 int key = setKey % Constant.ALPHABET.length;
-                char symbol = (char) read;
+                char symbol = Character.toLowerCase((char) read);
                 for (int i = 0; i < Constant.ALPHABET.length; i++) {
-                    if (symbol == Constant.ALPHABET[i]) {
-                        symbol = Constant.ALPHABET[i + key];
-                    }
+                        if (symbol == Constant.ALPHABET[i]) {
+                            if ((i + key) > Constant.ALPHABET.length - 1) {
+                                int i2 = (i + key) - Constant.ALPHABET.length;
+                                symbol = Constant.ALPHABET[i2];
+                                break;
+                            } else {
+                                symbol = Constant.ALPHABET[i + key];
+                                break;
+                            }
+                        }
                 }
+                bufferedWriter.write(symbol);
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new AppException(e);
         }
 
--- src/main/java/com/javarush/boyarinov/constats/Constant.java
+++ src/main/java/com/javarush/boyarinov/constats/Constant.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 public class Constant {
 
     private static final String RU = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-    private static final String SYMBOL = "\n.,\"':;-!? ";
+    private static final String SYMBOL = ".,\"':;-!? ";
     public static final char[] ALPHABET = (RU.toLowerCase() + SYMBOL).toCharArray();
     public static final String LOCATION_TXT = System.getProperty("user.dir") + File.separator + "text" + File.separator;
 
