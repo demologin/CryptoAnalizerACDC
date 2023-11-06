23335d78f53eb1fc6346b47e339a8bddefdd6723
OctaviaRa <149930464+OctaviaRa@users.noreply.github.com>
2023-11-06 17:57:10 +0300

    Вот

---
+++ src/main/java/com/javarush/kuzmina/resources/Alphabet.java
@@ -0,0 +1,13 @@
+package com.javarush.kuzmina.resources;
+
+import java.io.OptionalDataException;
+
+public class Alphabet {
+    public static OptionalDataException lowCaseAlph;
+
+    public static void alphabet(String[] args) {
+        char[] lowCaseAlph = new char[]{'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+        char[] upperCaseAlph = new char[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
+
+    }
+}
