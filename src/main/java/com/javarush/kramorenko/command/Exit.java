23bcc5105538132115bedf32a894f0b0a0f9044c
kramorenko <kramorenkoSE@gmail.com>
2023-11-12 23:55:59 +0800

    ADD Exit

---
+++ src/main/java/com/javarush/kramorenko/command/Exit.java
@@ -0,0 +1,9 @@
+package com.javarush.kramorenko.command;
+
+import com.javarush.kramorenko.constants.Const;
+
+public class Exit {
+    public void exit(){
+        System.out.println(Const.APPLICATION_CLOSED);
+    }
+}
